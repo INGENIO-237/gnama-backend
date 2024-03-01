c232555b1053b77ac00f1f6ccfaf06bb712a224a
INGENIO <ingeniokhalif@gmail.com>
2024-03-01 10:08:07 +0100

    corrected healthcheck typo

--- src/router.ts
+++ src/router.ts
@@ -2,7 +2,7 @@ import { Express, Request, Response } from "express";
 import UserRouter from "./routes/user.routes";
 
 export default function router(server: Express) {
-  server.get("/healtcheck", (req: Request, res: Response) =>
+  server.get("/healthcheck", (req: Request, res: Response) =>
     res.sendStatus(200)
   );
   server.use("/users", UserRouter);
