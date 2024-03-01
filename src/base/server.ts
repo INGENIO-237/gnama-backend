1177daf63daad0852bcc7850d7023cac4480b296
INGENIO <ingeniokhalif@gmail.com>
2024-03-01 18:09:49 +0100

    added configs for cloudinary

--- src/base/server.ts
+++ src/base/server.ts
@@ -2,6 +2,12 @@ import express from "express";
 import cors from "cors";
 import connectToDB from "./db-connection";
 import router from "../router";
+import { v2 as cloudinary } from "cloudinary";
+import {
+  CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET,
+  CLOUDINARY_CLOUD_NAME,
+} from "../config";
 
 export default function createServer() {
   const server = express();
@@ -12,6 +18,12 @@ export default function createServer() {
   (async () => {
     await connectToDB();
   })();
+
+  cloudinary.config({
+    cloud_name: CLOUDINARY_CLOUD_NAME as string,
+    api_key: CLOUDINARY_API_KEY as string,
+    api_secret: CLOUDINARY_API_SECRET as string,
+  });
 
   router(server);
 
--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,13 @@ import "dotenv/config";
 
 export const DB_URI = process.env.DB_URI;
 export const PORT = process.env.PORT || 7000;
+
+// Auth0
 export const AUTH0_AUDIENCE = process.env.AUTH0_AUDIENCE;
 export const AUTH0_ISSUER_BASE_URL = process.env.AUTH0_ISSUER_BASE_URL;
 export const AUTH0_TOKEN_SIGNING_ALG = process.env.AUTH0_TOKEN_SIGNING_ALG;
+
+// Cloudinary
+export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
+export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
+export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
